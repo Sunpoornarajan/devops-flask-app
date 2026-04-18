32fc4aedf959d9a3228913447bc5348259406510
Sunpoornarajan <sunpoornaraajan07@gmail.com>
2026-04-18 20:22:06 +0530

    Initial DevOps project

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+balance = 10000
+transactions = []
+
+@app.route("/")
+def home():
+    return render_template("index.html", balance=balance, transactions=transactions)
+
+@app.route("/deposit", methods=["POST"])
+def deposit():
+    global balance
+    amount = int(request.form["amount"])
+    balance += amount
+    transactions.append(f"Deposited ₹{amount}")
+    return redirect("/")
+
+@app.route("/transfer", methods=["POST"])
+def transfer():
+    global balance
+    amount = int(request.form["amount"])
+    receiver = request.form["receiver"]
+
+    if amount <= balance:
+        balance -= amount
+        transactions.append(f"Sent ₹{amount} to {receiver}")
+    else:
+        transactions.append("Insufficient Balance")
+
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
