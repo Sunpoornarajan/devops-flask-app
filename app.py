f62428b34b360045b00ca57c22af351bd233bd36
Sunpoornarajan_M <sunpoornarajan07@gmail.com>
2026-06-26 23:50:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # MySQL Database Configuration
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Rajan@06@localhost/banking_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bank.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # Initialize Database
